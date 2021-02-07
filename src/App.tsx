9ca0d1a6bb4529ca2f6bb72a6a323c9bb8f0114f
Yash Chauhan <yc9274@gmail.com>
2021-02-07 11:34:15 +0530

    first run

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          asdasdas
+          asdasd
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
